24099292e8ca703374789ec7ff61859e105a42b6
Deb Cassano <workerdebbee@gmail.com>
2019-07-15 23:25:29 -0400

    Add thanks.html, Modified index.html, main.css, scripts.js - thanks.html skeleton, links back to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,8 @@ function change() {
     document.body.style.background = rainbow[Math.floor(7 * Math.random())];
 }
 button.addEventListener("click", change);
+
+//To Do:
+//1.) When User clicks "Send Honey!" Button:
+    //*Add 100 "Pollen" to "Total Pollen" count
+    //*Display "Thank You!" Page
